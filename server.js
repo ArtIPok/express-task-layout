ce8172924c9d05a9e271b7e4878d29f3078ac37b
ArtIPok <apokora@itsap.pl>
2021-05-25 13:51:36 +0200

    Add conditioning handlebars

--- server.js
+++ server.js
@@ -43,16 +43,14 @@ app.get('/hello/:name', (req, res) => {
 
 app.post('/contact/send-message', (req, res) => {
 
-  const { author, sender, title, message } = req.body;
+  const { author, sender, title, message, design } = req.body;
 
-  if(author && sender && title && message) {
+  if(author && sender && title && message && design) {
     res.render('contact', { isSent: true });
   }
   else {
     res.render('contact', { isError: true });
   }
-
-  res.json(req.body);
 });
 
 app.use((req, res) => {
